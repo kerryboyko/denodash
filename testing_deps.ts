cd3945e896433df4e70be00b9a507bc5cd9dc35c
Brian Boyko <briboyko@publicisgroupe.net>
2021-03-11 23:07:23 +0000

    Cleanup

--- testing_deps.ts
+++ testing_deps.ts
@@ -1,5 +1,1 @@
 export { Rhum } from "https://deno.land/x/rhum@v1.1.7/mod.ts";
-
-export const falseyValues: any[] = [, null, undefined, false, 0, NaN, ""];
-
-export const primitives: any[] = [null, undefined, false, true, 1, NaN, "a"];
